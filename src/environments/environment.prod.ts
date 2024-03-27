afbab7863f5f9676cfb6a3d88d1a59fd0723b1bf
giancarlo.berninzon.2@gmail.com <40646643+gianpoker1@users.noreply.github.com>
2024-03-28 00:16:44 +0100

    Public-digital

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,9 +13,32 @@ export const environment = {
     registroUrl: 'http://104.248.32.80:8080/auth/registro',
     rolUrl: 'http://104.248.32.80:8080/api/roles',
     servicioDisponibleUrl: 'http://104.248.32.80:8080/api/serviciosDisponibles',
-    servicioSolicitadoUrl: '104.248.32.80:8080/api/serviciosSolicitados',
+    servicioSolicitadoUrl: 'http://104.248.32.80:8080/api/serviciosSolicitados',
     servicioUrl: 'http://104.248.32.80:8080/api/servicios',
     trabajadorUrl: 'http://104.248.32.80:8080/api/trabajadores',
     usuarioRolUrl: 'http://104.248.32.80:8080/api/usuarioRoles',
     usuarioUrl: 'http://104.248.32.80:8080/api/usuarios'
+
+
+    //LOCAL
+// production: true,
+//     authUrl : 'http://localhost:8080',
+//     categoriaUrl: 'http://localhost:8080/api/categorias',
+//     ciudadUrl: 'http://localhost:8080/api/ciudades',
+//     clienteUrl: 'http://localhost:8080/api/clientes',
+//     compraUrl: 'http://localhost:8080/api/compras',
+//     detalleServicioUrl: 'http://localhost:8080/api/detalleServicios',
+//     paisUrl: 'http://localhost:8080/api/paises',
+//     productoUrl: 'http://localhost:8080/api/productos',
+//     proveedorUrl: 'http://localhost:8080/api/proovedores',
+//     provinciaUrl: 'http://localhost:8080/api/provincias',
+//     registroUrl: 'http://localhost:8080/auth/registro',
+//     rolUrl: 'http://localhost:8080/api/roles',
+//     servicioDisponibleUrl: 'http://localhost:8080/api/serviciosDisponibles',
+//     servicioSolicitadoUrl: 'http://localhost:8080/api/serviciosSolicitados',
+//     servicioUrl: 'http://localhost:8080/api/servicios',
+//     trabajadorUrl: 'http://localhost:8080/api/trabajadores',
+//     usuarioRolUrl: 'http://localhost:8080/api/usuarioRoles',
+//     usuarioUrl: 'http://localhost:8080/api/usuarios'
+
 };
